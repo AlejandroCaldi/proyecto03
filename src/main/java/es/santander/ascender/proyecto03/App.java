bd5794986551fbec6704d96749e7bb5a4dc6d134
curso07 <alejandro.cald@gmail.com>
2024-11-07 10:48:03 +0100

    Agregad de generaciòn de arrays

--- src/main/java/es/santander/ascender/proyecto03/App.java
+++ src/main/java/es/santander/ascender/proyecto03/App.java
@@ -9,6 +9,11 @@ public class App {
         Calculadora calc = new Calculadora();
         calc.presentarTablaMultiplicar(8,20);
         int[] tabla = calc.calcularTablaMultiplicar(8, 10);
-        System.out.println(tabla);
+        //ystem.out.println(tabla);
+        int[] arrayOriginal = calc.generarArrayIncremental(99, true);
+        int[] arrayRevertido = calc.revertirArrayIncremental(arrayOriginal, true);
+
+        System.out.println(arrayRevertido);
+        }
     }
-}
+
--- src/main/java/es/santander/ascender/proyecto03/Calculadora.java
+++ src/main/java/es/santander/ascender/proyecto03/Calculadora.java
@@ -63,4 +63,56 @@ public class Calculadora {
         return resultados;
     }
 
+    public int[] generarArrayIncremental(int valorMaximo, boolean imprimir) {
+
+        int[] arrayGenerado = new int[valorMaximo+1];
+        
+        for (int i=0;i<=arrayGenerado.length-1;i++) {
+            arrayGenerado[i] = i;
+        }
+
+        if (imprimir) {
+
+            imprimirArray(arrayGenerado);
+        }
+
+        return arrayGenerado;
+
+    }
+
+    public int[] revertirArrayIncremental(int[] arrayInicial, boolean imprimir) {
+
+        int[] arrayNuevo = new int[arrayInicial.length];
+
+        int j = arrayInicial.length;
+        for(int i = 0; i<=arrayInicial.length-1;i++) {
+
+            arrayNuevo[i] = j - i;
+        }
+
+        if (imprimir) {
+
+            imprimirArray(arrayNuevo);
+        }
+
+        return arrayNuevo;
+    }
+
+        public void imprimirArray(int[] arrayImprimir) {
+
+            System.out.println("\n");
+            System.out.print("[");
+            for(int i=0; i<=arrayImprimir.length-1;i++) {
+
+                if (i<arrayImprimir.length-1) {
+                    System.out.print(arrayImprimir[i] + ",");
+                } else {
+                    System.out.print(arrayImprimir[i]);
+                }
+
+            }
+            System.out.print("]");
+            System.out.println("\n");
+        }
+
 }
